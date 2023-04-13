10b64f7da3529bbd72812a80fed089fe4b0f7598
katsu-17 <tsutsui.katsushi@gmail.com>
2023-04-14 00:08:31 +0900

    tabの削除

--- pokemon.rb
+++ pokemon.rb
@@ -1,20 +1,20 @@
 class Pokemon
-	def initialize(name, type1, type2, hp)
-		@name = name
-		@type1 = type1
-		@type2 = type2
-		@hp = hp
-	end
+  def initialize(name, type1, type2, hp)
+    @name = name
+    @type1 = type1
+    @type2 = type2
+    @hp = hp
+  end
 
-	def name
-		@name
-	end
+  def name
+    @name
+  end
 
-	def change_name(new_name)
-		if new_name == 'うんこ'
-			puts '不適切な名前です'
-		else
-			@name = new_name
-		end
-	end
+  def change_name(new_name)
+    if new_name == 'うんこ'
+      puts '不適切な名前です'
+    else
+      @name = new_name
+    end
+  end
 end
